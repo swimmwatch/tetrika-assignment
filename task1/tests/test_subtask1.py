46cb3ab63e6da9171f914f29703b0a155a54be84
Dmitry Vasiliev <contact.vasiliev.dmitry@gmail.com>
2022-07-05 15:13:25 +0300

    Fix testcases for subtask 1

--- task1/tests/test_subtask1.py
+++ task1/tests/test_subtask1.py
@@ -7,9 +7,9 @@ from task1.subtask1 import task
 
 
 @pytest.mark.parametrize('array,expected', [
-    ('10', 0),
-    ('110', 1),
-    ('111111111110000000000000000', 10)
+    ('10', 1),
+    ('110', 2),
+    ('111111111110000000000000000', 11)
 ])
 def test_subtask1(array: str, expected: int):
     actual = task(array)
